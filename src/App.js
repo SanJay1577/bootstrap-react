291c0c55e9179660afced3a6bfeca7ca3e520d19
sanjay <sanjayprashanth1997@gmail.com>
2023-02-27 10:30:49 +0530

    colud_deployment

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 useEffect(()=>{
    const getStudents = async () => {
     try {
-      const response = await fetch("http://localhost:9000/students", {
+      const response = await fetch("https://students-data-nu.vercel.app/students", {
         method:"GET"
       }); 
       const data = await response.json();
